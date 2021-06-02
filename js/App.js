9866681e79e3abf5054d6d16a72d7cc9afa3690c
alimh <amohamadi17@gmail.com>
2021-06-02 17:15:06 +0430

    'main'

--- js/App.js
+++ js/App.js
@@ -1,37 +1,63 @@
 //fucntions
 
 const showNumbers = (e) => {
-  if (e.target.classList.contains("opt")) {
-    //find Plus Index of show value
-    const plusIndex = show.value.lastIndexOf("+");
-    const minIndex = show.value.lastIndexOf("\u2212");
+  let lastChar = result.value.slice(-1);
 
+  if (e.target.classList.contains("opt")) {
     let condition = false;
 
+    //Display the number entered by the user
     show.value += e.target.textContent;
 
     if (e.target.classList.contains("btn-sm")) {
+      //If the user clicks on it, you will see the operation she wants to perform
       result.value = show.value;
     }
 
-    if (
-      result.value.slice(-1) === "+" ||
-      result.value.slice(-1) === "\u2212" ||
-      result.value.slice(-1) === "\u00D7" ||
-      result.value.slice(-1) === "\u00F7"
-    ) {
-      if (e.target.classList.contains("btn-sm")) {
-        show.value = show.value.slice(0, -1);
-        condition = true;
+    // If the last character entered was one of these
+    oprators.map((item) => {
+      if (result.value.slice(-1) === item) {
+        // And if one of them was clicked (Oprators)
+        if (e.target.classList.contains("btn-sm")) {
+          show.value = show.value.slice(0, -1);
+          condition = true;
+        }
       }
-    }
+    });
 
-    condition ? show.value = "" : null
+    // If the condition is correct, the displayed value will be empty
 
-    
-
+    condition ? (show.value = "") : null;
   }
 
+  // If an equal click was made
+  if (e.target.classList.contains("equl")) {
+    // If the last character was equal to plus
+    if (lastChar === "+") {
+      let resPlus = +result.value.slice(0, -1) + +show.value;
+      result.value = result.value + show.value + e.target.textContent;
+      show.value = resPlus;
+      // If the last character was equal to minus
+    } else if (lastChar === "\u2212") {
+      let resMin =
+        lastChar === "\u2212" ? +result.value.slice(0, -1) - +show.value : null;
+      result.value = result.value + show.value + e.target.textContent;
+      show.value = resMin;
+      // If the last character was equal to Multiplication
+    } else if (lastChar === "\u00D7") {
+      let resMulti =
+        lastChar === "\u00D7" ? +result.value.slice(0, -1) * +show.value : null;
+      result.value = result.value + show.value + e.target.textContent;
+      show.value = resMulti;
+      // And if the last character was equal to Divide
+    } else if (lastChar === "\u00F7") {
+      let res = +result.value.slice(0, -1) / +show.value;
+      result.value = result.value + show.value + e.target.textContent;
+      show.value = res;
+    }
+  }
+
+  // If you click on Clean, we will delete everything
   if (e.target.classList.contains("clean")) {
     show.value = "";
     result.value = "";
